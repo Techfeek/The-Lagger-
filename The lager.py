7b61152fa03c616816e8c51610169f04573b082f
Doan Goolsby <techfreek21@gmail.com>
2017-10-21 22:40:35 +0000

    Added are you redy screen and added comints

--- The lager.py
+++ The lager.py
@@ -1,5 +1,11 @@
 def line_s ():
     print ('------------------------------------------------------------------')
+#welcome screen 
 print ('Welcome to the lagger made by techfreek21')
 line_s()
 print ('Worniing this my kill or lag your programe')
+line_s()
+
+#This is the Are you ready screen 
+print ("Are you ready fore the lagg?")
+q1=input()
