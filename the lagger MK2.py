a06505583b6106a9b4184c984dc326c39d518852
Techfeek <techfreek21@gmail.com>
2017-10-21 16:31:03 -0700

    Added the lagger part

--- the lagger MK2.py
+++ the lagger MK2.py
@@ -1,1 +1,8 @@
+def line_s():
+  print("-------------------------------------------------------------------------------")
 print ('Hello and welcom to the lagger MK2')
+line_s()
+
+for lagger in  range (1,2000000):
+  print (lagger)
+  
