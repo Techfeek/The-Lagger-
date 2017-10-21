06ef9aee24ba4db2046b9c9083bb90d48387271d
Techfeek <techfreek21@gmail.com>
2017-10-21 16:37:22 -0700

    New Worning

--- the lagger MK2.py
+++ the lagger MK2.py
@@ -2,7 +2,8 @@ def line_s():
   print("-------------------------------------------------------------------------------")
 print ('Hello and welcom to the lagger MK2')
 line_s()
-
+print ("Worning may lag or break computer")
+line_s()
 for lagger in  range (1,2000000):
   print (lagger)
   
