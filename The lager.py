ce36be9f9a3587163b836443195850ff251bd659
Doan Goolsby <techfreek21@gmail.com>
2017-10-21 22:34:20 +0000

    New worning

---
+++ The lager.py
@@ -0,0 +1,5 @@
+def line_s ():
+    print ('------------------------------------------------------------------')
+print ('Welcome to the lagger made by techfreek21')
+line_s()
+print ('Worniing this my kill or lag your programe')
