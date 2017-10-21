0cc7a3534480dda2b0ad76eecf4abb5a2cab0f69
Techfeek <techfreek21@gmail.com>
2017-10-21 16:30:02 -0700

    Rong branch

--- the lagger MK2.py
+++
@@ -1,8 +0,0 @@
-def line_s():
-  print("-------------------------------------------------------------------------------")
-print ('Hello and welcom to the lagger MK2')
-line_s()
-
-for lagger in  range (1,2000000):
-  print (lagger)
-  
